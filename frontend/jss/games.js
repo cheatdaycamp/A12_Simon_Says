754980e866b8b678a845d21ed5870e9a0903fad1
Aaron Gilman <gilmanaa2@gmail.com>
2019-07-27 18:25:04 +0300

    updates for filters

--- frontend/jss/games.js
+++ frontend/jss/games.js
@@ -23,3 +23,38 @@ gamesBack.click(function () {
             window.location = response.url
         })
 })
+
+window.addEventListener("load", function () {
+    var openGames = document.getElementsByClassName("game shadowed");
+    for (var l = 0; l < openGames.length; l++) {
+        if (openGames[l].classList.contains("open")) {
+            console.log("open");
+        } else {
+            openGames[l].classList.add("hide-game");
+        }
+    }
+});
+
+var filters = document.getElementsByClassName("filter clickable");
+for (var i = 0; i < filters.length; i++) {
+    filters[i].addEventListener("click", function (e) {
+        var filterButtons = document.getElementsByClassName("filter clickable");
+        for (var k = 0; k < filters.length; k++) {
+            if (filterButtons[k].classList.contains("filter-selected")) {
+                filterButtons[k].classList.remove("filter-selected");
+            }
+        }
+        e.target.classList.toggle("filter-selected");
+        var allGames = document.getElementsByClassName("game shadowed");
+        var currentFilter = e.target.textContent.toLowerCase();
+        for (var j = 0; j < allGames.length; j++) {
+            if (currentFilter == "all") {
+                allGames[j].classList.remove("hide-game")
+            } else if (allGames[j].classList.contains(currentFilter)) {
+                allGames[j].classList.remove("hide-game")
+            } else {
+                allGames[j].classList.add("hide-game")
+            }
+        }
+    })
+}
