c8f9bf875d152507c7256b46219f1ab57151158c
Aaron Gilman <gilmanaa2@gmail.com>
2019-07-27 17:14:35 +0300

    heroku working

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
 import os
-from bottle import run, TEMPLATE_PATH, debug
+from bottle import run, TEMPLATE_PATH
 
 from backend.handlers import app
 from backend.static_handlers import staticHandler
 from backend.page_handlers import pageHandler
-from sys import argv
-
-#DEBUG = os.environ.get("DEBUG")
-#debug(True)
 
 TEMPLATE_PATH.insert(0, os.path.dirname(__file__))
 
 app.merge(staticHandler)
 app.merge(pageHandler)
-
-#run(app, host='localhost', port=os.environ.get('PORT', 5000))
-
-#if DEBUG:
-#    run(app, host='localhost', port=os.environ.get('PORT', 5000))
-#else:
-#    run(app, host='0.0.0.0', port=argv[1])
 
 if os.environ.get('APP_LOCATION') == 'heroku':
     run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
