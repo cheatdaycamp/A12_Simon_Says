0b0d073adfa90de11458a3fc971372ff796c35a7
Aaron Gilman <gilmanaa2@gmail.com>
2019-07-27 16:15:17 +0300

    heroku updates attempt1

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import os
-from bottle import run, TEMPLATE_PATH
+from bottle import run, TEMPLATE_PATH, debug
 
 from backend.handlers import app
 from backend.static_handlers import staticHandler
 from backend.page_handlers import pageHandler
+from sys import argv
+
+DEBUG = os.environ.get("DEBUG")
+debug(True)
 
 TEMPLATE_PATH.insert(0, os.path.dirname(__file__))
 
@@ -11,3 +15,8 @@ app.merge(staticHandler)
 app.merge(pageHandler)
 
 run(app, host='localhost', port=os.environ.get('PORT', 5000))
+
+if DEBUG:
+    run(app, host='localhost', port=os.environ.get('PORT', 5000))
+else:
+    run(app, host='0.0.0.0', port=argv[1])
