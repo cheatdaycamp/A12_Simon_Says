6329a50255a435f7e7bb9625a4c5b26413a74d4c
Aaron Gilman <gilmanaa2@gmail.com>
2019-07-27 16:35:19 +0300

    heroku attempt3

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ from backend.static_handlers import staticHandler
 from backend.page_handlers import pageHandler
 from sys import argv
 
-DEBUG = os.environ.get("DEBUG")
-debug(True)
+#DEBUG = os.environ.get("DEBUG")
+#debug(True)
 
 TEMPLATE_PATH.insert(0, os.path.dirname(__file__))
 
 app.merge(staticHandler)
 app.merge(pageHandler)
 
-#run(app, host='localhost', port=os.environ.get('PORT', 5000))
+run(app, host='localhost', port=os.environ.get('PORT', 5000))
 
-if DEBUG:
-    run(app, host='localhost', port=os.environ.get('PORT', 5000))
-else:
-    run(app, host='0.0.0.0', port=argv[1])
+#if DEBUG:
+#    run(app, host='localhost', port=os.environ.get('PORT', 5000))
+#else:
+#    run(app, host='0.0.0.0', port=argv[1])
